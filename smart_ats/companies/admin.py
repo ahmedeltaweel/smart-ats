7cdca176cce2c1be47e533f5935ccee177df31aa
tata <taher.mostafa808@gmail.com>
2020-10-22 10:17:45 +0000

    iss#4 Create Company Admin

--- smart_ats/companies/admin.py
+++ smart_ats/companies/admin.py
@@ -1,3 +1,72 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import CompanyAdmin , Company
+from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
 
-# Register your models here.
+
+
+@admin.register(CompanyAdmin)
+class CompanyAdminSite(UserAdmin):
+    model = CompanyAdmin
+
+    list_display = ["username",'email','company']
+
+    fieldsets = (
+        ('Personal info', {'fields': ('first_name','last_name','username','email',)}),
+        ('Company info',{'fields':('company','user_type')}),
+        ("Additional info", {'fields': ('is_active','is_staff','date_joined','last_login',)}),
+                )
+    
+    readonly_fields =['password','date_joined','last_login','user_type']
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('first_name','last_name','username','email', 'password1', 'password2',
+                        'company','is_active','is_staff',
+            )
+            }
+        ),)
+
+    def save_model(self, request, obj, form, change):
+        obj.user_type = 'admin'
+        super().save_model(request, obj, form, change)
+
+
+class StackedCompanyAdmin(admin.TabularInline):
+    model = CompanyAdmin
+    fields = ('username','email','is_active')
+    readonly_fields = ('username', 'email')
+    extra = 0
+
+    def has_add_permission(self, request, *args, **kwargs):
+        return False
+
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    model = Company
+
+    list_display = ('id', 'name', 'Comapany_website','Company_Admin', 'is_active')
+
+    list_filter = ('is_active',)
+
+    search_fields = ("name__icontains", )
+
+    inlines = (StackedCompanyAdmin,)
+
+
+    def Comapany_website(self, obj):
+        return format_html(f'<a href="{obj.website}">Website</a>')
+
+
+    def Company_Admin(self, obj):
+        url = str(reverse("admin:companies_companyadmin_changelist")
+                +"?"
+                + urlencode({"company__id": f"{obj.id}"}))
+        
+        return format_html(f"<a href='{url}'>company admins</a>")        
+    
+    Company_Admin.short_description = "Company Admins"
--- smart_ats/companies/models.py
+++ smart_ats/companies/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
-
 from smart_ats.users.models import User
 
 
 class CompanyAdmin(User):
-    company = models.ForeignKey("companies.Company", on_delete=models.CASCADE)
+    company = models.ForeignKey("companies.Company", on_delete=models.CASCADE, related_name='admins')
 
+
+    class Meta :
+        verbose_name = 'Company Admin'
 
 class Company(TimeStampedModel):
     name = models.CharField(max_length=15, blank=False, null=False)
     website = models.URLField(blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     address = models.TextField(blank=False, null=False)
+    is_active = models.BooleanField(default=True)
+
 
     def __str__(self):
         return self.name
+
+    class Meta :
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
