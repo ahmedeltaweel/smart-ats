f09b65daa318537f375abcd40c4d95118d5b9a75
tata <taher.mostafa808@gmail.com>
2020-10-26 09:49:37 +0000

    iss#4 add company admin

--- smart_ats/companies/models.py
+++ smart_ats/companies/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
+
 from smart_ats.users.models import User
 
 
 class CompanyAdmin(User):
-    company = models.ForeignKey("companies.Company", on_delete=models.CASCADE, related_name='admins')
+    company = models.ForeignKey(
+        "companies.Company", on_delete=models.CASCADE, related_name="admins"
+    )
 
+    class Meta:
+        verbose_name = "Company Admin"
 
-    class Meta :
-        verbose_name = 'Company Admin'
 
 class Company(TimeStampedModel):
     name = models.CharField(max_length=15, blank=False, null=False)
@@ -17,11 +20,9 @@ class Company(TimeStampedModel):
     address = models.TextField(blank=False, null=False)
     is_active = models.BooleanField(default=True)
 
-
     def __str__(self):
         return self.name
 
-    class Meta :
-        verbose_name = 'Company'
-        verbose_name_plural = 'Companies'
-
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
