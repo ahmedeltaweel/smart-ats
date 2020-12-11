1e52e75b0717d865e872e886b2c52028086c78e6
EmanAly16 <eng.emanaly16@gmail.com>
2020-12-11 16:04:12 +0200

    add jobs app

--- smart_ats/jobs/models.py
+++ smart_ats/jobs/models.py
@@ -2,25 +2,24 @@ from django.db import models
 from model_utils import Choices
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
-from smart_ats.companies.models import *
+from smart_ats.companies.models import Company, CompanyAdmin
 from taggit.managers import TaggableManager
 
 
 class Category(TimeStampedModel):
-    title = models.CharField(max_length=30,db_index=True)
+    title = models.CharField(max_length=30, db_index=True)
 
 
 class Job(TimeStampedModel):
-    STATUS = Choices(('Active', _('Active')),('Draft', _('Draft')),('Archived', _('Archived')))
-      
+    STATUS = Choices(('Active', _('Active')), ('Draft', _('Draft')), ('Archived', _('Archived')))
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    author = models.ForeignKey(CompanyAdmin, on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey(CompanyAdmin, on_delete=models.CASCADE)
     status = models.CharField(max_length=15, choices=STATUS, default=STATUS.Archived)
     tags = TaggableManager()
 
     def __str__(self):
-        return self.name
-
+        return f'{self.name} {self.company.name} {self.author.name}'
