c2e9441b4c1588640897783b40f01b54b56cc0f4
Eman <eng.emanaly16@gmail.com>
2021-04-17 02:05:52 +0200

    fsm

--- config/settings/base.py
+++ config/settings/base.py
@@ -62,6 +62,7 @@ DJANGO_APPS = [
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
+    "django_fsm",
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
--- smart_ats/jobs/models.py
+++ smart_ats/jobs/models.py
@@ -7,23 +7,36 @@ from taggit.managers import TaggableManager
 
 from .managers import JobManager
 
+from django_fsm import FSMField, transition
+
 
 class Category(TimeStampedModel):
     name = models.CharField(max_length=30, db_index=True)
 
 
+STATUS = ('Active', 'Draft', 'Archived')
+STATUS = list(zip(STATUS, STATUS))
 class Job(TimeStampedModel):
-    STATUS = Choices(('Active', _('Active')), ('Draft', _('Draft')), ('Archived', _('Archived')))
 
     title = models.CharField(max_length=100)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     author = models.ForeignKey(CompanyAdmin, on_delete=models.CASCADE)
-    status = models.CharField(max_length=15, choices=STATUS, default=STATUS.Draft)
     tags = TaggableManager()
 
     objects = JobManager()
+    state = FSMField(default=STATUS[1], choices=STATUS)
+
+    @transition(field=state, source='Draft', target='Active')
+    def start(self):
+
+        pass
+    @transition(field=state, source='Active', target='Archived')
+    def start(self):
+
+        pass
+
 
     def __str__(self):
         return f'{self.title} {self.company.name} {self.author.first_name}'
