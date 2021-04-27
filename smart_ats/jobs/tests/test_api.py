08c48f857cfd8a710b86e46913802a32d47516a9
Elsaeed Ahmed <elsaeedahmed97@gmail.com>
2021-04-27 21:06:10 +0200

    add tests

--- smart_ats/jobs/tests/test_api.py
+++ smart_ats/jobs/tests/test_api.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 
@@ -11,9 +12,14 @@ class JobAPITestCase(APITestCase):
         self.client = APIClient(HTTP_AUTHORIZATION="Token " + token[0].key)
         self.api_path = "/api/jobs/"
 
-    def test_jobs_list(self):
+    def test_jobs_list_authenticated(self):
         response = self.client.get(f"{self.api_path}", content_type="application/json")
         self.assertEqual(response.status_code, 200)
+
+    def test_company_list_unauthenticated(self):
+        client = APIClient()
+        response = client.get(self.api_path, content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_job_list_data(self):
         response = self.client.get(f"{self.api_path}", content_type="application/json")
@@ -21,13 +27,3 @@ class JobAPITestCase(APITestCase):
         self.assertEqual(response.data[0]["description"], self.job.description)
         self.assertEqual(response.data[0]["category"], f"{self.job.category}")
         self.assertEqual(response.data[0]["state"], self.job.state)
-
-    def test_job_detail_retreive(self):
-        response = self.client.get(
-            "{}{}/".format(self.api_path, self.job.id),
-            content_type="application/json",
-        )
-        self.assertEqual(response.data["title"], self.job.title)
-        self.assertEqual(response.data["description"], self.job.description)
-        self.assertEqual(response.data["category"], f"{self.job.category}")
-        self.assertEqual(response.data["state"], self.job.state)
