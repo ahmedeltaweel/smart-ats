3281c7a5228e020c6bc4487198eececa07c7bf93
Marc <Marcoatef222@gmail.com>
2021-04-27 17:08:51 +0200

    Altering The Serializer

--- smart_ats/jobs/api/views.py
+++ smart_ats/jobs/api/views.py
@@ -1,18 +1,33 @@
 from rest_framework import permissions, viewsets
-
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
 from smart_ats.jobs.models import Job
-
-from .permissions import UpdateOwnJob
-from .serializers import JobSerializer
+from .permissions import IsJobCompanyAdminOrReadOnly
+from .serializers import JobSerializer, JobWriterSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class JobViewSet(viewsets.ModelViewSet):
-    serializer_class = JobSerializer
     queryset = Job.objects.all()
     permission_classes = (
-        UpdateOwnJob,
-        IsAuthenticated
+        IsJobCompanyAdminOrReadOnly
         )
-   
+
+    def get_serializer(self):
+        if self.action in ["list", "retrieve"]:
+            return JobSerializer
+        elif self.action in ["create", "update"]:
+            return JobWriterSerializer
+
+    @action(detail=True, methods=["PATCH"])
+    def activate(self, request, pk=None):
+        job = self.get_object()
+        job.activate()
+        job.save()
+        return Response({"message": "Ok"})
+
+    @action(detail=True, methods=["PATCH"])
+    def archive(self, request, pk=None):
+        job = self.get_object()
+        job.archive()
+        job.save()
+        return Response({"message": "Ok"})
