182a0df37c0d3e1c27914a400e39020c87eec4c5
Eman <eng.emanaly16@gmail.com>
2021-04-28 21:39:15 +0200

    test for retreive and create

--- smart_ats/jobs/tests/test_api.py
+++ smart_ats/jobs/tests/test_api.py
@@ -4,6 +4,7 @@ from rest_framework.test import APIClient, APITestCase
 from django.contrib.auth import get_user_model
 
 from .factories import JobFactory
+from smart_ats.companies.tests.factories import CompanyFactory
 
 User = get_user_model()
 
@@ -48,3 +49,69 @@ class JobAPITestCase(APITestCase):
         user = APIClient(HTTP_AUTHORIZATION="Token " + token[0].key)
         response = user.delete(f"{self.api_path}", content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_job_detail_retreive(self):
+        response = self.client.get(
+            "{}{}/".format(self.api_path, self.job.id),
+            content_type="application/json",
+        )
+        self.assertEqual(response.data["title"], self.job.title)
+        self.assertEqual(response.data["description"], self.job.description)
+        self.assertEqual(response.data["category"], f"{self.job.category}")
+        self.assertEqual(response.data["state"], self.job.state)
+
+    def test_job_detail_retreive_by_ananymous(self):
+        client = APIClient()
+        response = client.get(
+            "{}{}/".format(self._api_path, self.job.id),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_job_create_by_admin(self):
+        category = CategoryFactory.create()
+        data = {
+            "title": "title1",
+            "description": "temp",
+            "category": category.id,
+            "company": self.job.company.id,
+            "author": "name",
+            "state": "DRAFT",
+            "tags": "",
+        }
+        response = self.client.post(
+            "{}".format(self._api_path),
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["title"], "title1")
+        self.assertEqual(response.data["description"], "temp")
+        self.assertEqual(response.data["category"], category.id)
+        self.assertEqual(response.data["company"], self.job.company.id)
+        self.assertEqual(response.data["author"], "name")
+        self.assertEqual(response.data["state"], "DRAFT")
+        self.assertEqual(response.data["tags"], "")
+
+    def test_company_create_by_ananymous(self):
+        data = {
+            "title": "no title",
+            "description": "no temp",
+            "category": "",
+            "company": "LOL",
+            "author": "anan",
+            "state": "DRAFT",
+            "tags": "",
+        }
+        client = APIClient()
+        response = client.post(
+            "{}".format(self._api_path),
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 403)
+
+            
+
+
+
