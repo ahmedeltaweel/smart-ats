ffd89e8c1cbaa7183c9c018bf8a5c5a375171b5f
Eman <eng.emanaly16@gmail.com>
2021-05-12 16:38:10 +0200

    Add permission

--- smart_ats/companies/api/permissions.py
+++ smart_ats/companies/api/permissions.py
@@ -1,14 +1,27 @@
+from smart_ats.companies.models import User, CompanyAdmin
 from rest_framework import permissions
 
 
 class IsCompanyAdminOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        if request.user.user_type != User.statuses.admin:
+            return False
+    
+        if request.method == "POST":
+            if hasattr(request.user, CompanyAdmin):
+                return False
+        
+        return True
+
+
+    def has_object_permission(self, request, view, obj):
+        
         if not hasattr(request.user, "companyadmin"):
             return False
-        # check for action if create return True
-        if request.method == "POST" and not obj:
-            return True
+       
         # update, delete
         return request.user.companyadmin in obj.company_admin.all()
