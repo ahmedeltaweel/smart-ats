cf41686c9a3b60669dc50fd722d300f950c5a847
Marc <Marcoatef222@gmail.com>
2021-05-12 15:31:57 +0200

    try parser and celery

---
+++ smart_ats/jobs/tasks.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task
+from pyresparser import ResumeParser
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def cv(pdf):
+    data = ResumeParser(pdf).get_extracted_data()
+
+    for key, value in data.items():
+        print(key, ':', value)
+    # return data
