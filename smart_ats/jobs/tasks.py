28da06bb5fed7a1978d22b08cb9c5655ac30d97e
Marc <Marcoatef222@gmail.com>
2021-05-17 21:38:38 +0200

    Alter task and test

--- smart_ats/jobs/tasks.py
+++ smart_ats/jobs/tasks.py
@@ -1,18 +1,26 @@
-from __future__ import absolute_import, unicode_literals
-
 from celery import shared_task
+# from config.celery_app import app
 from pyresparser import ResumeParser
+import urllib
+import os
+from .models import JobApplication
 
 
 @shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def cv(pdf):
-    data = ResumeParser(pdf).get_extracted_data()
-
-    for key, value in data.items():
-        print(key, ":", value)
+def cv(url):
+    response = urllib.request.urlopen(url)
+    if url[-3:] == 'pdf':
+        file = open("resume.pdf", 'wb')
+        file.write(response.read())
+        file.close()
+        data = ResumeParser("resume.pdf").get_extracted_data()
+        os.remove("resume.pdf")
+    else:
+        file = open("resume.txt", 'wb')
+        file.write(response.read())
+        file.close()
+        data = ResumeParser("resume.txt").get_extracted_data()
+        os.remove("resume.txt")
+    JobApplication.data = data
+    JobApplication.save()
     return data
---
+++ smart_ats/jobs/tests/test_cv_parser.py
@@ -0,0 +1,9 @@
+from .factories import JobApplicationFactory
+from django.test import TestCase
+from smart_ats.jods.tasks import cv
+
+
+class TestCvParserData(TestCase):
+    def test_success_extract_data(self):
+        job_application = JobApplicationFactory.create()
+        self.assertEqual(cv(job_application.cv_url), job_application.data)
