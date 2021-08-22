826ff4bc39d387d71179e912c07abf4852951c13
TAHER-MOSTAFA <taher.mostafa808@gmail.com>
2021-08-23 00:33:31 +0200

    Add push to registry all tags

--- tag_images.sh
+++ tag_images.sh
@@ -4,4 +4,5 @@ images=$(cat local.yml | grep 'smartats.azurecr.io/smart_ats' | cut -d ":" -f 2)
 for image in $images
 do
   docker tag $image $image:${GITHUB_SHA:-latest}
+  docker push -a $image
 done
